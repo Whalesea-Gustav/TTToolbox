69b6a3baa1c9efca5e44a6412699558e5d0ae5e7
Jim <40590729+Whalesea-Gustav@users.noreply.github.com>
2024-06-08 19:12:39 +0800

    Add Anim Modifier to Copy Notifies and Curves

---
+++ Source/TTToolbox/Private/TTCopyAllCurvesAndNotifiesAnimModifier.cpp
@@ -0,0 +1,63 @@
+// The MIT License (MIT)
+// ---------------------
+// 
+// Copyright 2022 Achim Turan (https://www.instagram.com/tuatec/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include "TTCopyAllCurvesAndNotifiesAnimModifier.h"
+
+// Unreal Engine includes
+#include "AnimationBlueprintLibrary.h"
+#include "Animation/AnimSequence.h"
+
+void UTTCopyAllCurvesAndNotifiesAnimModifier::OnApply_Implementation(UAnimSequence* TargetSequence)
+{
+  // check input arguments
+  if (!IsValid(TargetSequence) || !IsValid(SourceSequence))
+  {
+    UE_LOG(LogTemp, Error, TEXT("Called \"OnApply_Implementation\" without valid \"TargetSequence\" or valid \"SourceSequence\"."));
+    return;
+  }
+
+  if (ReplaceExistingCurves)
+  {
+    UAnimationBlueprintLibrary::RemoveAllCurveData(TargetSequence);
+  }
+
+  // copy all curves to the target anim sequence
+  for (auto& sourceCurve : SourceSequence->GetCurveData().FloatCurves)
+  {
+    // remove the curve if it already exists ==> overwrite
+    if (UAnimationBlueprintLibrary::DoesCurveExist(TargetSequence, sourceCurve.GetName(), ERawCurveTrackTypes::RCT_Float))
+    {
+      UAnimationBlueprintLibrary::RemoveCurve(TargetSequence, sourceCurve.GetName());
+    }
+
+    // introduce the curve
+    UAnimationBlueprintLibrary::AddCurve(TargetSequence, sourceCurve.GetName());
+
+    // prepare needed objects to set the curve keys
+    const FAnimationCurveIdentifier curveId(sourceCurve.GetName(), ERawCurveTrackTypes::RCT_Float);
+
+    // transfer curve keys
+    TargetSequence->GetController().SetCurveKeys(curveId, sourceCurve.FloatCurve.GetConstRefOfKeys());
+  }
+
+  // Copy All Notifies to the target anim sequence
+  UAnimationBlueprintLibrary::CopyAnimNotifiesFromSequence(SourceSequence, TargetSequence, ReplaceExistingNotifies);
+  
+}
